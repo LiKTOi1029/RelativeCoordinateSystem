c5271d6be65d58f27e45c9a726b62c2d1c437fb5
LiKTOi1029 <testingboy5689@gmail.com>
2025-10-14 20:34:52 +0330

    RCS.lua wrapping up

--- RCS.lua
+++ RCS.lua
@@ -1,14 +1,60 @@
 RCS = {}
 RCS.__index = RCS
 
-Coordinates = {["X"] = 0, ["Y"] = 0}
+RCS.Coordinates = {["X"] = 0, ["Y"] = 0, ["Z"] = 0}
 
-function RCS.forward()
-	turtle.forward()
+Switch = 0
+function DirectionUpdater(Boolean)
+	if Switch == 3 and Boolean then
+		Switch = 0
+	elseif Switch == 0 and not Boolean then
+		Switch = 3
+	elseif Boolean then
+		Switch = Switch + 1
+	else
+		Switch = Switch - 1
+	end
+end
+function CoordinatesUpdater(AmIGoingAlongTheYAxis)
+	if not AmIGoingAlongTheYAxis then
+		if Switch == 0 then
+			RCS.Coordinates["X"] = RCS.Coordinates["X"] + 1
+		elseif Switch == 1 then
+			RCS.Coordinates["Z"] = RCS.Coordinates["Z"] + 1
+		elseif Switch == 2 then
+			RCS.Coordinates["X"] = RCS.Coordinates["X"] - 1
+		else
+			RCS.Coordinates["Z"] = RCS.Coordinates["Z"] - 1
+		end
+	elseif AmIGoingAlongTheYAxis == 1 then
+		RCS.Coordinates["Y"] = RCS.Coordinates["Y"] + 1
+	elseif AmIGoingAlongTheYAxis == -1 then
+		RCS.Coordinates["Y"] = RCS.Coordinates["Y"] - 1
+	end
 end
 function RCS.turnLeft()
 	turtle.turnLeft()
+	DirectionUpdater(false)
 end
 function RCS.turnRight()
 	turtle.turnRight()
+	DirectionUpdater(true)
 end
+function RCS.forward()
+	turtle.forward()
+	CoordinatesUpdater(false)
+end
+function RCS.back()
+	turtle.forward()
+	CoordinatesUpdater(false)
+end
+function RCS.up()
+	turtle.forward()
+	CoordinatesUpdater(1)
+end
+function RCS.down()
+	turtle.forward()
+	CoordinatesUpdater(-1)
+end
+
+return RCS
