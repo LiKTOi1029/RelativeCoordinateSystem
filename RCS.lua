088f8345c26371f3615629e37eb65fcb60c62301
LiKTOi1029 <testingboy5689@gmail.com>
2025-10-14 20:13:22 +0330

    RCS.lua creating first movement custom movement functions

---
+++ RCS.lua
@@ -0,0 +1,14 @@
+RCS = {}
+RCS.__index = RCS
+
+Coordinates = {["X"] = 0, ["Y"] = 0}
+
+function RCS.forward()
+	turtle.forward()
+end
+function RCS.turnLeft()
+	turtle.turnLeft()
+end
+function RCS.turnRight()
+	turtle.turnRight()
+end
