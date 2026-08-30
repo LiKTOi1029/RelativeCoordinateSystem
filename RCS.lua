23458dda876e3e81841e1b5e9161b0d206ee6a3e
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-31 00:47:51 +0330

    RCS.lua adding test code

--- RCS.lua
+++ RCS.lua
@@ -1,4 +1,5 @@
 local FileName = "RCS.lua"
+local SaveFile = "Coordinates.RCSSV"
 local RCS = {}
 local Coordinates
 local FacingDirection = 0
@@ -6,8 +7,10 @@ local Dir
 local Absolute
 
 function RCS.Init()
+  Dir = shell.dir()
   Absolute = shell.resolve(FileName)
-  print(Absolute)
+  if fs.exists(Dir ..  "/" .. SaveFile) then print("yah") else print("nah") end
+  print(Dir, Absolute)
 end
 
 function RCS.UpdateCoordintes()
