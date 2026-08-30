576e4370a331ac67f42dd170e9aa705eb99d5049
LiKTOi1029 <testingboy5689@gmail.com>
2026-08-30 23:59:06 +0330

    RCS.lua adding all essentials

---
+++ RCS.lua
@@ -0,0 +1,27 @@
+local RCS
+local Coordinates
+local FacingDirection = 0
+local Dir
+local Absolute
+
+function RCS.Init()
+  Dir = shell.dir()
+  Absolute = shell.resolve(Dir)
+  print(Absolute)
+end
+
+function RCS.UpdateCoordintes()
+  
+end
+
+function RCS.Move(Direction)
+  
+end
+
+function RCS.Turn(Direction)
+  
+end
+
+RCS.Init()
+
+return RCS
